c846299403297ba75475f9a1c61f6a6d5bb2a477
Tinkershlok <161268083+Tinkershlok@users.noreply.github.com>
2025-03-08 15:47:29 -0500

    @ 3:46 We updates the css of the website and made it look good, it is now working with the About and the different tabs

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ import genapi
 
 app = Flask(__name__)
 
+@app.route('/about.html', methods=['GET'])
+def about():
+    return render_template('about.html')
+
 @app.route('/', methods=['POST', 'GET'])
 def main():
     return render_template('main.html')
-"""
-@app.route('/newschecker', methods=['POST'])
+
+@app.route('/newschecker.html', methods=['POST','GET'])
 def check_news():
-    user_message = request.json.get("message")
-    if not user_message:
-        return jsonify({"error": "No message provided"}), 400
     return render_template('newschecker.html')
-"""
 
 if __name__=="__main__":
     #app.run(host='0.0.0.0', port=os.environ.get('FLASK_PORT', 5000), debug=os.environ.get('FLASK_DEBUG', True))
