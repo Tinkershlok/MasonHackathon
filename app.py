8602e5aeacd14348aa6c4dda07b45e78e9b460a8
Tinkershlok <161268083+Tinkershlok@users.noreply.github.com>
2025-03-08 16:46:37 -0500

    @ 4:47 Added the Complete Gemini and starting to code for Beautiful Soup
    Co-Authored-By: Flypiksel <160438774+Flypiksel@users.noreply.github.com>
    Co-Authored-By: great12theworld <88846533+great12theworld@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for, redirect, request, jsonify
 from datetime import datetime
-"""
+import genapi
 import json
+"""
 import os
 from google import generativeai
-import google.generativeai as genai
-import genapi
+import google.generativeai as genai"
 """
+from google import genai
+
 
 app = Flask(__name__)
 
@@ -18,8 +20,18 @@ def about():
 def main():
     return render_template('main.html')
 
-@app.route('/newschecker.html', methods=['POST','GET'])
+    
+
+@app.route("/newschecker", methods=["POST", "GET"])
 def check_news():
+    user_message = request.json.get("message")
+    
+    if not user_message:
+        return jsonify({"error": "No message provided"}), 400
+
+    ai_response = genapi.getresponse(user_message)
+    
+    return jsonify({"ai_message": ai_response})
     return render_template('newschecker.html')
 
 if __name__=="__main__":
--- genapi.py
+++ genapi.py
@@ -1,20 +1,18 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=['AIzaSyCM4uL_WXs76v7-cIFfuyrCY_OUhhkkY6A'])
-model = genai.GenerativeModel("gemini-pro")
-response = model.generate_content("what is Figma.")
-print(response.text)
-
-"""
-client = genai.Client(api_key="AIzaSyCM4uL_WXs76v7-cIFfuyrCY_OUhhkkY6A")
+client = genai.Client(
+    api_key="AIzaSyCM4uL_WXs76v7-cIFfuyrCY_OUhhkkY6A"
+    )
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="What is Figma"
 )
+
+response
+
 print(response.text)
-"""
+
 
